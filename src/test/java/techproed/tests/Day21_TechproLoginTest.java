5f9961da0e67a90374460965bc11205163712fff
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-29 00:05:00 +0300

    29/01/2023

--- src/test/java/techproed/tests/Day21_TechproLoginTest.java
+++ src/test/java/techproed/tests/Day21_TechproLoginTest.java
@@ -11,9 +11,10 @@ public class Day21_TechproLoginTest {
 
     //https://testcenter.techproeducation.com/index.php?page=form-authentication
     //Page object Model kullanarak sayfaya giriş yapildigini test edin
+    //Sayfadan cikis yap ve cikis yapildigini test et
 
     @Test
-    public void testtest() {
+    public void techproTest() {
         Driver.getDriver().get(ConfigReader.getProperty("techproed_url"));
 
         TechproLoginPage techproLoginPage = new TechproLoginPage();
@@ -38,7 +39,7 @@ public class Day21_TechproLoginTest {
     }
 
 
-    //Sayfadan cikis yap ve cikis yapildigini test et
+
     //techproed
     //SuperSecretPassword
     //TechproLoginPage
