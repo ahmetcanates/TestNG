442276a6cf0bbe7506eae5d94d1037c3ceae52c1
Ahmet Can Ates <omerzed54@gmail.com>
2023-02-06 00:07:30 +0300

    05/02/2023

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -2,10 +2,12 @@ package techproed.utilities;
 
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
-public class ListenersRetry implements IRetryAnalyzer {
-    private int retryCount = 0;
-    private static final int maxRetryCount = 1;
 
+public class ListenersRetry implements IRetryAnalyzer {
+    //    BU SINIF SADECE FAIL OLAN TEST CASELERE TEKRAR CALISTIRIR
+    private int retryCount = 0;
+    //    maxRetryCount EK OLARAK CALISMA SAYISI. BU ORNEKDE FAIL OLNA TESTLER EXTRA 1 KEZ DAHA CALISIR
+    private static final int maxRetryCount = 1;
     @Override
     public boolean retry(ITestResult result) {
         if (retryCount < maxRetryCount) {
@@ -14,5 +16,4 @@ public class ListenersRetry implements IRetryAnalyzer {
         }
         return false;
     }
-
 }
