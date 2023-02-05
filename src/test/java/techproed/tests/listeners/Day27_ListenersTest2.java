f0bbafede2a022b5f0a9e4d066939ff42a4fb85b
Ahmet Can Ates <omerzed54@gmail.com>
2023-02-05 23:56:16 +0300

    05/02/2023

--- src/test/java/techproed/tests/listeners/Day27_ListenersTest2.java
+++ src/test/java/techproed/tests/listeners/Day27_ListenersTest2.java
@@ -7,33 +7,28 @@ import org.testng.annotations.Test;
 import techproed.utilities.Driver;
 
 public class Day27_ListenersTest2 {
-
     @Test
     public void test1(){
         System.out.println("PASS");
         Assert.assertTrue(true);
     }
-
     @Test
     public void test2(){
         System.out.println("FAIL");
-        //        throw new NoSuchElementException("No Such Element Exception");
+//        throw new NoSuchElementException("No Such Element Exception");
         Driver.getDriver().get("https://www.amazon.com");
-        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));
+        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));//FAIL ETSIN
     }
-
     @Test
     public void test3(){
         System.out.println("SKIP");
         throw new SkipException("Metotu Atla");
     }
-
     @Test
     public void test4(){
         System.out.println("EXCEPTION");
 //        throw new NoSuchElementException("No Such Element Exception");
         Driver.getDriver().get("https://www.techproeducation.com");
-        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));
+        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));//FAIL ETSIN
     }
-
 }
